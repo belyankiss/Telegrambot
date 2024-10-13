f1700b358f4d9655b65b17ddabcc1d64bc7565e6
belyankiss <belyankiss@gmail.com>
2024-10-13 11:45:16 +0300

    Переделаны классы сообщений. Возможно будет дополнен или переделан еще раз

--- proxy_bot/constants/msg_constants.py
+++ proxy_bot/constants/msg_constants.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from datetime import datetime
 from typing import Optional, Union
 
@@ -16,82 +17,88 @@ class ShortButton:
     BUY_WITH_DISCOUNT = "📉 Купить со скидкой {discount}%"
 
 
+class CreatorMessages(ABC):
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        self.size = 1
 
-class CreatorMessages:
-    def __init__(self,
-                 text: Optional[str] = None,
-                 buttons: Optional[Union[dict, list]] = None,
-                 size: Optional[int] = 1):
-        self.text: str = text or self._create_text()
-        self.buttons: Optional[Union[dict, list]] = buttons or self._create_buttons()
-        self.size = size
+    @abstractmethod
+    def text(self):
+        pass
 
-    def _create_text(self, *args, **kwargs) -> str:
-        return ""
-
-    def _create_buttons(self, *args, **kwargs) -> Optional[Union[dict, list]]:
-        return []
+    @abstractmethod
+    def buttons(self):
+        pass
 
     def __repr__(self):
         return f'{self.text}\n{self.buttons}\n{self.size}'
+
+    def __call__(self):
+        return {"text": self.text(), "buttons": self.buttons(), "size": self.size}
 
 
 #  messages for start page
 
 class Start(CreatorMessages):
-    def __init__(self, username: Optional[str]):
-        self.username = username
-        super().__init__(size=2)
-
-    def _create_text(self) -> str:
-        return (f"👋 Привет, {self.username}\n\n"
+    def text(self) -> str:
+        return (f"👋 Привет, {self.kwargs['username']}\n\n"
                 "<u>❗Обязательно ознакомься с правилами нашего магазина, можешь прочитать</u> "
                 f"<a href='{settings.URL_RULES}'>👉 "
                 "ТУТ</a>")
 
-    def _create_buttons(self) -> Optional[Union[dict, list]]:
+    def buttons(self) -> Optional[Union[dict, list]]:
+        self.size = 2
         return ['👨🏼‍💻 Профиль', '🔐 Аренда прокси',
                 '🔤 Уникализатор', '🧑‍💻 Промокоды']
 
 
 class UserNotMember(CreatorMessages):
-    def __init__(self, text: Optional[str] = None):
-        text = text or "👁 Для пользования ботом, тебе необходимо подписаться на канал ниже! 👇"
-        super().__init__(text=text)
+    def text(self):
+        return "👁 Для пользования ботом, тебе необходимо подписаться на канал ниже! 👇"
 
-    def _create_buttons(self, *args, **kwargs) -> Optional[Union[dict, list]]:
+    def buttons(self):
         return {f'Подписаться {settings.CHAT_NAME}': settings.CHAT_SUB_LINK,
                 '✅ Подписался': 'check_subscribe'}
 
 
 class UserNotSubscribe(UserNotMember):
-    def __init__(self):
-        text = "❌ Вы меня не обманете! Тебе необходимо подписаться на канал ниже для пользования ботом! 👇"
-        super().__init__(text=text)
+    def text(self):
+        return "❌ Вы меня не обманете! Тебе необходимо подписаться на канал ниже для пользования ботом! 👇"
+
+    def buttons(self):
+        super().buttons()
 
 
 class IfUserBlocked(CreatorMessages):
-    def __init__(self):
-        text = "❌ Администрация заблокировала вас в боте!"
-        super().__init__(text=text)
+    def text(self):
+        return "❌ Администрация заблокировала вас в боте!"
+
+    def buttons(self):
+        return None
 
 
 class ForAdminAfterRegistration(CreatorMessages):
-    def __init__(self):
-        text = ("❗️Пользователь @{username}\n\n"
+    def text(self):
+        username = self.kwargs['username']
+        return (f"❗️Пользователь @{username}\n\n"
                 "Только что зарегистрировался в боте!\n"
                 f"{datetime.now().strftime('%d-%m-%Y %H:%M')}")
-        super().__init__(text=text)
+
+    def buttons(self):
+        return None
 
 
 class AdminPanel(CreatorMessages):
-    def __init__(self):
-        text = "<b>🏦 МЕНЮ АДМИНИСТРАТОРА 🏦</b>"
-        buttons = ['📨 Сделать рассылку', '❓ Информация о пользователе',
+    def text(self):
+        return "<b>🏦 МЕНЮ АДМИНИСТРАТОРА 🏦</b>"
+
+    def buttons(self):
+        self.size = 2
+        return ['📨 Сделать рассылку', '❓ Информация о пользователе',
                    '📊 Статистика бота', '👑 Добавить администратора',
                    '💰 Добавить промокод', '⚙ Управление промокодами',
                    '⚙ Настройки фото']
-        super().__init__(text=text, buttons=buttons, size=2)
 
 
 # _______________________________________________________________________________________________________________________
@@ -99,63 +106,59 @@ class AdminPanel(CreatorMessages):
 # messages for profile page
 
 class MainProfile(CreatorMessages):
-    def __init__(self, user_data: dict):
-        self.user_data = user_data
-        buttons = {
+
+    def text(self):
+        user_data = self.kwargs['user_data']
+        return (f"<b>Профиль пользователя @{user_data['username']}\n\n"
+                f"🏦 Ваш баланс: {user_data['balance']} RUB\n\n"
+                f"🤌 Количество покупок: {user_data['purchase_count']}\n\n"
+                f"💸 Доход от рефералов: {user_data['referral_income']}\n\n"
+                f"👥 Всего ваших рефералов: {user_data['referral_count']}\n\n"
+                f"🙋🏻‍♂️ Ваш id: {user_data['user_id']}\n\n"
+                f"♨ Ваша реферальная ссылка: <code>{settings.HREF_REF}{user_data['user_id']}</code></b>\n\n"
+                f"🤑 <b>Вы будете получать на баланс {settings.PERCENTAGE_FROM_REF * 100}% от пополнений "
+                "ваших рефералов!</b>")
+
+    def buttons(self):
+        return {
             '🗄 Ваши покупки': 'purchases',
             '💰 Пополнить баланс': 'payment',
             '🗣 Написать администратору': 'msg_admin',
             '🏦 Сотрудничество и реклама': 'advertising'
         }
-        super().__init__(buttons=buttons)
-
-    def _create_text(self):
-        return (f"<b>Профиль пользователя @{self.user_data['username']}\n\n"
-                f"🏦 Ваш баланс: {self.user_data['balance']} RUB\n\n"
-                f"🤌 Количество покупок: {self.user_data['purchase_count']}\n\n"
-                f"💸 Доход от рефералов: {self.user_data['referral_income']}\n\n"
-                f"👥 Всего ваших рефералов: {self.user_data['referral_count']}\n\n"
-                f"🙋🏻‍♂️ Ваш id: {self.user_data['user_id']}\n\n"
-                f"♨ Ваша реферальная ссылка: <code>{settings.HREF_REF}{self.user_data['user_id']}</code></b>\n\n"
-                f"🤑 <b>Вы будете получать на баланс {settings.PERCENTAGE_FROM_REF * 100}% от пополнений "
-                "ваших рефералов!</b>")
 
 
 class UserPurchasesPage(CreatorMessages):
-    def __init__(self, list_purchases: list):
-        self.list_purchases: list = list_purchases
-        super().__init__()
-
-    def _create_buttons(self):
-        buttons = {}
-        self.size = 1
-        if len(self.list_purchases) > 10:
-            self.size = 2
-            self.list_purchases = self.list_purchases[-10:]
-        for item, purchase in enumerate(self.list_purchases):
-            buttons[f'{item + 1}. {purchase.purchase_time.strftime("%d-%m-%Y")}'] = f'purchase_user:{purchase.id}'
-        return buttons | ShortButton.BACK_PROFILE
-
-    def _create_text(self):
-        if self.list_purchases:
+    def text(self):
+        if self.kwargs['list_purchases']:
             return "<b>Вот ваши последние покупки.</b>"
         return "<b>У вас не было покупок... 😢</b>"
 
+    def buttons(self):
+        list_purchases = self.kwargs['list_purchases']
+        buttons = {}
+        self.size = 1
+        if len(list_purchases) > 10:
+            self.size = 2
+            list_purchases = list_purchases[-10:]
+        for item, purchase in enumerate(list_purchases):
+            buttons[f'{item + 1}. {purchase.purchase_time.strftime("%d-%m-%Y")}'] = f'purchase_user:{purchase.id}'
+        return buttons | ShortButton.BACK_PROFILE
+
+
+
 
 class ForSinglePurchase(CreatorMessages):
-    def __init__(self, data_purchase: dict):
-        buttons = {ShortButton.BACK: 'purchases'}
-        self.d_p: dict = data_purchase
-        super().__init__(buttons=buttons)
 
-    def _create_text(self):
-        if len(self.d_p) == 8:
+    def text(self):
+        data_purchase = self.kwargs['data_purchase']
+        if len(data_purchase) == 8:
             return ("Товар: {product_name}\n\n"
                     "Цена: {amount} RUB\n"
                     "Страна: {country}\n"
                     "Город: {city}\n"
                     "Данные: <code>{host}:{port}:{username}:{password}</code>"
-                    ).format(**self.d_p)
+                    ).format(**data_purchase)
         else:
             return ("Товар: {product_name}\n\n"
                     "Цена: {amount} RUB\n"
@@ -163,17 +166,21 @@ class ForSinglePurchase(CreatorMessages):
                     "Дата покупки: {purchase_time}\n"
                     "Дата окончания: {end_time}\n\n"
                     "HTTP: <code>{host}:{port}:{username}:{password}</code>\n"
-                    "SOCKS5: <code>{host}:{port_socks}:{username}:{password}</code>").format(**self.d_p)
+                    "SOCKS5: <code>{host}:{port_socks}:{username}:{password}</code>").format(**data_purchase)
+
+    def buttons(self):
+        return {ShortButton.BACK: 'purchases'}
 
 
 class ChoosePayment(CreatorMessages):
-    def __init__(self):
-        text = "<b>Вы берите систему для оплаты</b>"
-        buttons = {'🪙 CryptoBot': 'cryptobot',
-                   '💶 YooMoney': 'yoo_money',
-                   '🚀 XRocket': 'xrocket',
+    def text(self):
+        return "<b>Вы берите систему для оплаты</b>"
+
+    def buttons(self):
+        return {'🪙 CryptoBot': 'cryptobot',
+                '💶 YooMoney': 'yoomoney',
+                '🚀 xRocket': 'xrocket',
                    ShortButton.BACK: "profile"}
-        super().__init__(text=text, buttons=buttons)
 
 
 class WriteToAdministration:
@@ -181,6 +188,24 @@ class WriteToAdministration:
     about_adv = '<b>Напишите сообщение насчет сотрудничества</b>'
     message_send = '<b>Сообщение отправлено!</b>'
     back_profile = ShortButton.BACK_PROFILE
+
+
+# _______________________________________________________________________________________________________________________
+
+# payments page
+
+class ChoicePayment:
+    cryptobot = ('<b>♻️ Оплата будет производиться через</b> <a href="https://t.me/CryptoBot">CryptoBot</a> \n'
+                 '<b>❗ Валюта для оплаты:</b> <i>RUB</i>\n\n'
+                 '✍️ <b>Введите сумму пополнения:</b>')
+    yoomoney = ('<b>♻️ Оплата будет производиться через</b> <a href="https://yoomoney.ru">YooMoney</a> \n'
+                '<b>❗ Валюта для оплаты:</b> <i>RUB</i>\n\n'
+                '✍️ <b>Введите сумму пополнения:</b>')
+    xrocket = ('<b>♻️ Оплата будет производиться через</b> <a href="https://t.me/@tonRocketBot">xRocket</a> \n'
+               '<b>❗ Валюта для оплаты:</b> <i>RUB</i>\n\n'
+               '✍️ <b>Введите сумму пополнения:</b>')
+    back_keyboard = {ShortButton.BACK: 'payment'}
+
 
 
 
