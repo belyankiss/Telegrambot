904483fd800da4a17e44cecde9624f33fc4ba1fd
belyankiss <belyankiss@gmail.com>
2024-10-14 12:47:28 +0300

    Добавлена обработка искдючения при взаимодействии с иксрокет

--- proxy_bot/complex_classes/payments/exceptions.py
+++ proxy_bot/complex_classes/payments/exceptions.py
@@ -1,10 +1,16 @@
 class UnderZeroError(Exception):
     def __init__(self):
-        self.text = "Вы ввели сумму равную или меньше нуля. Попробуйте еще раз!"
+        self.text = "<b><i>Вы ввели сумму равную или меньше нуля. Попробуйте еще раз!</i></b>"
         super().__init__(self.text)
 
 
 class WrongAmountError(Exception):
     def __init__(self):
         self.text = "<b>Необходимо ввести число!</b>\n\n<i>100 или 100.0</i>"
+        super().__init__(self.text)
+
+
+class XRocketException(Exception):
+    def __init__(self):
+        self.text = "<b>С сервисом xRocket неполадки, попробуйте позже!</b>"
         super().__init__(self.text)
