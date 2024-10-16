4cd3f0d15135d27011c007796c0bf30b2d290f4c
belyankiss <belyankiss@gmail.com>
2024-10-16 12:48:24 +0300

    Хэндлер для работы со скидками

---
+++ proxy_bot/handlers/user/discount_handler.py
@@ -0,0 +1,42 @@
+from aiogram import Router, F
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.types import Message
+
+from proxy_bot.complex_classes.discounts.discount_class import ActivateDiscount
+from proxy_bot.complex_classes.discounts.exceptions import (NotDiscountTextError,
+                                                            DiscountNotInBaseError,
+                                                            UserHaveDiscountError,
+                                                            DiscountWasActivatedError,
+                                                            NotActivatesError)
+from proxy_bot.constants.msg_constants import GetDiscountNameMessage, DiscountActivateSuccess
+from proxy_bot.custom_sender.send_class import SendUser
+
+discount_router = Router()
+
+
+class Discount(StatesGroup):
+    discount = State()
+
+
+@discount_router.message(F.text == '🧑‍💻 Промокоды')
+async def discount_1(msg: Message, state: FSMContext):
+    await state.clear()
+    await state.set_state(Discount.discount)
+    await SendUser(**GetDiscountNameMessage()())(msg)
+
+
+@discount_router.message(Discount.discount)
+async def discount_2(msg: Message, state: FSMContext):
+    await state.clear()
+    try:
+        discount = ActivateDiscount(msg)
+        percentage = await discount.activate()
+        text = DiscountActivateSuccess(percentage=percentage).text()
+    except (NotDiscountTextError,
+            DiscountNotInBaseError,
+            UserHaveDiscountError,
+            DiscountWasActivatedError,
+            NotActivatesError) as e:
+        text = e.text
+    await SendUser(text=text, delete=True)(msg)
