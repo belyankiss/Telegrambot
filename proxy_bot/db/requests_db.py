5f15b023f461fb1084d8522408061130872eea71
belyankiss <belyankiss@gmail.com>
2024-10-19 12:39:03 +0300

    Добавлены взаимодействия с бд, если прокси не рабочий, то его вносит в бд как нерабочий

--- proxy_bot/db/requests_db.py
+++ proxy_bot/db/requests_db.py
@@ -2,13 +2,13 @@ from datetime import datetime
 from typing import Optional, Union, List, Tuple
 
 from aiogram.types import Message, CallbackQuery
-from sqlalchemy import func, select
+from sqlalchemy import func, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from proxy_bot.constants.msg_constants import ForAdminAfterRegistration, ShortButton
 from proxy_bot.custom_sender.send_class import SendAdmins
 from proxy_bot.db.create_async_session import async_session
-from proxy_bot.db.models import User, Purchase, Discount, Admin, ProxyWork, Photo
+from proxy_bot.db.models import User, Purchase, Discount, Admin, ProxyWork, Photo, ProxySort
 from proxy_bot.helpers import countries_dict
 
 
@@ -18,12 +18,17 @@ class BaseSession:
 
 
 class UserORM(BaseSession):
-    def __init__(self, event: Optional[Union[Message, CallbackQuery]] = None):
+    def __init__(self,
+                 event: Optional[Union[Message, CallbackQuery]] = None,
+                 user_id: Optional[int] = None,
+                 username: Optional[str] = None):
         super().__init__()
-        self.user_id: int = event.from_user.id
-        self.username: Optional[str] = event.from_user.username
+        self.user_id: int = event.from_user.id if event is not None else user_id
+        self.username: Optional[str] = event.from_user.username if event is not None else username
         if isinstance(event, Message):
             self.referral_id: Optional[int] = (int(event.text.split()[1]) if "/start " in event.text else None)
+        else:
+            self.referral_id = None
         self.date = datetime.now()
 
         self.purchases = PurchaseORM(self.user_id) if self.user_id is not None else None
@@ -112,6 +117,14 @@ class UserORM(BaseSession):
                 user.activated_list += f"{name_discount},"
             await session.commit()
 
+    async def minus_balance(self, price: Union[int, float]) -> User:
+        async with self.session() as session:
+            user = await self.is_exist_user(session)
+            user.balance -= price
+            await session.commit()
+            await session.refresh(user)
+            return user
+
 
 
 class PurchaseORM:
@@ -131,7 +144,7 @@ class PurchaseORM:
     async def get_one_purchase(session: AsyncSession, purchase_id: int) -> dict:
         result: Purchase = await session.scalar(select(Purchase).where(Purchase.id.is_(purchase_id)))
         product_name = getattr(ShortButton, result.product_type)
-        if result.product_type == 'work':
+        if result.product_type == 'work' or result.product_type == 'sort':
             data = {
                 "product_name": product_name,
                 "amount": result.amount,
@@ -159,6 +172,39 @@ class PurchaseORM:
 
         return data
 
+    async def save_purchase(self, proxy: Union[ProxyWork, ProxySort], amount: int):
+        async with async_session() as session:
+            purchase_time = datetime.now()
+            end_time = None
+            port_socks = None
+            if isinstance(proxy, ProxySort):
+                product_type = "sort"
+            else:
+                product_type = "work"
+            purchase = Purchase(user_id=self.user_id,
+                                host=proxy.host,
+                                port=proxy.port,
+                                port_socks=port_socks,
+                                password=proxy.password,
+                                username=proxy.username,
+                                country=proxy.country,
+                                city=proxy.city,
+                                purchase_time=purchase_time,
+                                end_time=end_time,
+                                amount=amount,
+                                product_type=product_type
+                                )
+            session.add(purchase)
+            await session.commit()
+
+    @staticmethod
+    async def save_proxyline_purchase(proxy: Purchase):
+        async with async_session() as session:
+            session.add(proxy)
+            await session.commit()
+
+
+
 
 class DiscountORM(BaseSession):
     def __init__(self):
@@ -175,14 +221,40 @@ class DiscountORM(BaseSession):
             await session.commit()
 
 
-class ProxyWorkORM(BaseSession):
-    def __init__(self):
+class Proxies(BaseSession):
+    def __init__(self, tablename: str):
         super().__init__()
+        if tablename == "ProxyWork":
+            self.table = ProxyWork
+        else:
+            self.table = ProxySort
 
     async def get_proxies(self):
         async with self.session() as session:
-            return list(await session.scalars(select(ProxyWork)
-                                              .where(ProxyWork.work.is_(True) & ProxyWork.used.is_(False))))
+            return list(await session.scalars(select(self.table)
+                                              .where(self.table.work.is_(True) & self.table.used.is_(False))))
+
+    async def update_busy_proxy(self, proxy_id: int):
+        async with self.session() as session:
+            await session.execute(update(self.table).where(self.table.id.is_(proxy_id)).values(used=True))
+            await session.commit()
+
+    async def get_bad_proxy(self):
+        async with self.session() as session:
+            work = list(await session.scalars(select(ProxyWork).where(ProxyWork.work.is_(False))))
+            sort = list(await session.scalars(select(ProxySort).where(ProxySort.work.is_(False))))
+            return work, sort
+
+    async def add_bad_proxy(self, proxy_id: int):
+        async with self.session() as session:
+            await session.execute(update(self.table).where(self.table.id.is_(proxy_id)).values(work=False))
+            await session.commit()
+
+    async def kill_proxy(self):
+        async with self.session() as session:
+            await session.execute(update(self.table).values(work=False))
+            await session.commit()
+
 
 
 class AdminsORM:
