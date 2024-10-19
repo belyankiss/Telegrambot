0215e78d8c56dc15518e997ba3f4aa0e004e6ad7
belyankiss <belyankiss@gmail.com>
2024-10-19 12:35:56 +0300

    Тут изменений не было

--- proxy_bot/main.py
+++ proxy_bot/main.py
@@ -25,7 +25,6 @@ async def main() -> None:
                        discount_router,
                        unique_router,
                        )
-    print(Constant.ADMINS, len(Constant.PROXIES_WORK))
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)  # Запуск long-polling
 
